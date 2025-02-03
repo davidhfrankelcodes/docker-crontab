2411de252954311de3969532b4eeda08b58ca09d
David Frankel <davidhfrankelcodes@gmail.com>
2025-02-02 21:11:13 -0600

    removed example

--- scripts/jenkins-rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATE_STRING=$(date)
-echo ${DATE_STRING}
-echo "Begin jenkins-rebuild.sh"
-FULL_CMD="export GIT_HOME=/home/david/Git && cd /home/david/Git/docker-jenkins && docker compose pull && docker compose up -d --force-recreate && docker system prune -af"
-
-ssh david@optiplex.local.davidhfrankel.com "${FULL_CMD}"
-echo "Complete"
